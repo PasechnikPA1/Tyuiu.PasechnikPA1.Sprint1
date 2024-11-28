a5982e9cc58a67a1937a6d78883186038a54c2b6
Pavel Pasechnik <ingle1337@gmail.com>
2024-11-28 23:44:45 +0500

    task2

---
+++ Tyuiu.PasechnikPA1.Sprint1.Task2.V9.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.PasechnikPA1.Sprint1.Task2.V9.Lib
+{
+    public class DataService : ISprint1Task2V9
+    {
+        public double CalculateVolumeCircle(int r)
+        {
+            return 4.0 / 3.0 * 3.14 * (r * r * r);
+        }
+    }
+}
---
+++ Tyuiu.PasechnikPA1.Sprint1.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PasechnikPA1.Sprint1.Task2.V9.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint1.Task2.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.CalculateVolumeCircle(x);
+            Assert.AreEqual(33.493333333333333, res);
+        }
+    }
+}
