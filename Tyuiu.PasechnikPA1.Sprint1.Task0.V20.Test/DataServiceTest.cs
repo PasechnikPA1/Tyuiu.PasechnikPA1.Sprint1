a14dd7933842f6ef6136d4686fe85415d86dbb51
Pavel Pasechnik <ingle1337@gmail.com>
2024-11-28 18:21:01 +0500

    Add project files.

---
+++ Tyuiu.PasechnikPA1.Sprint1.Task0.V20.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PasechnikPA1.Sprint1.Task0.V20.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint1.Task0.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(13, res);
+        }
+    }
+}
---
+++ Tyuiu.PasechnikPA1.Sprint1.Task0.V20/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.PasechnikPA1.Sprint1.Task0.V20.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint1.Task0.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Пасечник П. А. | АСОиУБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Пасечник Павел Андреевич | АСОиУБ-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение  10 + 9/3               *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 10 + 9/3                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
