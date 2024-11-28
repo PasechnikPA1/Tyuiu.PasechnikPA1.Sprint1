a93d6a0d893f5be724e9c8e3fdac08925fd94b73
Pavel Pasechnik <ingle1337@gmail.com>
2024-11-29 00:01:02 +0500

    task2

---
+++ Tyuiu.PasechnikPA1.Sprint1.Task2.V17.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PasechnikPA1.Sprint1.Task2.V17.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint1.Task2.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 130;
+            var res = ds.ConvertMinutesToHours(x);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
--- Tyuiu.PasechnikPA1.Sprint1.Task2.V17/Program.cs
+++ Tyuiu.PasechnikPA1.Sprint1.Task2.V17/Program.cs
@@ -1,5 +1,5 @@
-﻿using Tyuiu.PasechnikPA1.Sprint1.Task2.V9.Lib;
-namespace Tyuiu.PasechnikPA1.Sprint1.Task2.V9
+﻿using Tyuiu.PasechnikPA1.Sprint1.Task2.V17.Lib;
+namespace Tyuiu.PasechnikPA1.Sprint1.Task2.V17
 {
     internal class Program
     {
@@ -9,33 +9,48 @@ namespace Tyuiu.PasechnikPA1.Sprint1.Task2.V9
 
             Console.Title = "Спринт #1 | Выполнил: Пасечник П. А. | АСОиУБ-24-1";
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Спринт#1                                                                *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
             Console.WriteLine("* Задание #2                                                              *");
-            Console.WriteLine("* Вариант 9                                                               *");
+            Console.WriteLine("* Вариант #17                                                             *");
             Console.WriteLine("* Выполнил: Пасечник Павел Андреевич | АСОиУБ-24-1                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("* вычисляет результат по формуле (x * y / 2) + 10                         *");
+            Console.WriteLine("* и печатает его на экране.                                               *");
+            Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            int x;
 
-            Console.WriteLine("Радиус шара(целое число):");
-            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество минут: ");
+            string input = Console.ReadLine();
 
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
 
 
-            Console.WriteLine("Объем шара(вещественное число) = " + ds.CalculateVolumeCircle(x));
 
-            Console.ReadLine();
+
+            if (int.TryParse(input, out int minutes))
+            {
+
+                int hours = minutes / 60;
+
+
+                Console.WriteLine($"Полное количество часов: {hours}");
+            }
+            else
+            {
+
+                Console.WriteLine("Ошибка: введите целое число.");
+                Console.ReadLine();
+            }
         }
     }
 }
